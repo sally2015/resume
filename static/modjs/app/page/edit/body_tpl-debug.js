1a92bd1e0820ffb26aed663a00278598dc6713fe
sally2015 <740100506@qq.com>
2016-11-26 15:25:51 +0800

    内容模块

--- static/modjs/app/page/edit/body_tpl-debug.js
+++ static/modjs/app/page/edit/body_tpl-debug.js
@@ -3,13 +3,52 @@ define(function(require, exports, module) { 'use strict'
 		util = require('util/1.0.x/util'),
 		MX = require('/vuex/mapmixin');
 
-	// #ajax
+	// #ajax basic
 	var data = {
 		name: 'oby',
 		location: 'guangzhou',
 		phone: '18888888888',
 		email: '1234455@qq.com'
 	}
+	//ajax content
+	var moduleList = {
+			education: {
+				title:"教育背景",
+				arr:{
+					time: "201309-201706",
+					name: "广东工业大学",
+					others:"信息管理与信息系统",
+					des:"这是描述"
+				}
+			},
+			exp: {
+				title:"项目经验",
+				arr:{
+					time: "201309-201706",
+					name: "XXX项目",
+					others:"补充一下",
+					des:"这是描述"
+				}
+			},
+			work: {
+				title:"工作经验",
+				arr:{
+					time: "201309-201706",
+					name: "XXX公司",
+					others:"补充一下",
+					des:"这是描述"
+				}
+			},
+			skill: {
+				title:"专业技能",
+				arr:{
+					// time: "",
+					// name: "XXX公司",
+					// others:"补充一下",
+					des:"这是描述"
+				}
+			}
+		};
 
 	var computedMix = util.extend(MX.mapState, MX.mapGetters);
 
@@ -23,8 +62,7 @@ define(function(require, exports, module) { 'use strict'
 					phone: data.phone,
 					email: data.email
 				},
-				currentBasicBg: ''
-				
+				moduleList: moduleList
 			}
 		},
 		computed: computedMix,
